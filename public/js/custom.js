bd27ee767db46734859ca607642630f78609f812
fad12 <fad_2k6@hotmail.com>
2014-03-06 15:00:12 +0300

    first commit

---
+++ public/js/custom.js
@@ -0,0 +1,12 @@
+/*============================================================================
+                      ALL CUSTOM JS FOR THE SITE
+============================================================================*/
+jQuery(document).ready(function($) {
+        $(".royalSlider").royalSlider({
+            // options go here
+            // as an example, enable keyboard arrows nav
+            keyboardNavEnabled: true,
+            usePreloader:  true,
+            sliderDrag:true,
+        });  
+    });
